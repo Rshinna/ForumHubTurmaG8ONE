60314fb3eb212f0cfba43e12b2af27cf49a81bb7
Rodrigo Franco <rshina@hotmail.com>
2025-07-10 20:26:06 -0300

    criação de paginação e teste de funcionalidade

--- ForumHub/src/main/java/br/com/alura/ForumHub/controller/TopicoController.java
+++ ForumHub/src/main/java/br/com/alura/ForumHub/controller/TopicoController.java
@@ -1,23 +1,24 @@
 package br.com.alura.ForumHub.controller;
 
-import br.com.alura.ForumHub.repository.CursoRepository;
 import br.com.alura.ForumHub.domain.curso.Curso;
 import br.com.alura.ForumHub.domain.topico.Topico;
-import br.com.alura.ForumHub.repository.TopicoRepository;
+import br.com.alura.ForumHub.domain.usuario.Usuario;
 import br.com.alura.ForumHub.dto.requestDTO.TopicoRequestDTO;
 import br.com.alura.ForumHub.dto.responseDTO.TopicoResponseDTO;
-import br.com.alura.ForumHub.domain.usuario.Usuario;
 import br.com.alura.ForumHub.infra.erros.ErroDTO;
+import br.com.alura.ForumHub.repository.CursoRepository;
+import br.com.alura.ForumHub.repository.TopicoRepository;
 import br.com.alura.ForumHub.repository.UsuarioRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/topicos")
@@ -61,7 +62,61 @@ public class TopicoController {
 
     }
 
+    @GetMapping
+    public ResponseEntity<Page<TopicoResponseDTO>> listar(
+            @PageableDefault(size = 10, sort = "dataCriacao")Pageable paginacao
+            ) {
+        Page<Topico> pagina = topicoRepository.findAll(paginacao);
 
+        Page<TopicoResponseDTO> resposta = pagina.map(topico -> new TopicoResponseDTO(
+                        topico.getId(),
+                        topico.getTitulo(),
+                        topico.getMensagem(),
+                        topico.getCurso().getNome(),
+                        topico.getAutor().getNome(),
+                        topico.getDataCriacao(),
+                        topico.getStatus().name()
+                ));
+
+        return ResponseEntity.ok(resposta);
+    }
+
+    @GetMapping("/recentes")
+    public ResponseEntity<List<TopicoResponseDTO>> listarRecentes() {
+        List<Topico> topicos = topicoRepository.findTop10ByOrderByDataCriacaoAsc();
+        List<TopicoResponseDTO> resposta = topicos.stream()
+                .map(topico -> new TopicoResponseDTO(
+                        topico.getId(),
+                        topico.getTitulo(),
+                        topico.getMensagem(),
+                        topico.getCurso().getNome(),
+                        topico.getAutor().getNome(),
+                        topico.getDataCriacao(),
+                        topico.getStatus().name()
+                )).toList();
+
+        return ResponseEntity.ok(resposta);
+    }
+
+    @GetMapping("/buscar")
+    public ResponseEntity<List<TopicoResponseDTO>> buscarPorCursoEAno(
+            @RequestParam String nomeCurso,
+            @RequestParam int ano
+    ) {
+        List<Topico> topicos = topicoRepository.buscarPorCursoEAno(nomeCurso, ano);
+        List<TopicoResponseDTO> resposta = topicos.stream()
+                .map(topico -> new TopicoResponseDTO(
+                        topico.getId(),
+                        topico.getTitulo(),
+                        topico.getMensagem(),
+                        topico.getCurso().getNome(),
+                        topico.getAutor().getNome(),
+                        topico.getDataCriacao(),
+                        topico.getStatus().name()
+                )).toList();
+
+        return ResponseEntity.ok(resposta);
+    }
 
 
 }
--- ForumHub/src/main/java/br/com/alura/ForumHub/repository/TopicoRepository.java
+++ ForumHub/src/main/java/br/com/alura/ForumHub/repository/TopicoRepository.java
@@ -2,7 +2,16 @@ package br.com.alura.ForumHub.repository;
 
 import br.com.alura.ForumHub.domain.topico.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TopicoRepository extends JpaRepository<Topico,Long> {
     boolean existsByTituloAndMensagem(String titulo, String mensagem);
+
+    List<Topico> findTop10ByOrderByDataCriacaoAsc();
+
+    @Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso AND FUNCTION('YEAR', t.dataCriacao) = :ano")
+    List<Topico> buscarPorCursoEAno(@Param("nomeCurso") String nomeCurso, @Param("ano") int ano);
 }
